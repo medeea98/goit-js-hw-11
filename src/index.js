14d29f316559634773aa096e9fc950e1cf233551
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-02-11 01:06:34 +0200

    lightbox integrated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox'; 
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("search-form");
