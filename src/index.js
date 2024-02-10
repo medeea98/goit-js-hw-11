8c363bd8d4287fefa9f5209eec35dccb953d188f
medeea98 <142827871+medeea98@users.noreply.github.com>
2024-02-11 00:24:57 +0200

    restructuring

--- src/index.js
+++ src/index.js
@@ -65,46 +65,27 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("createPhotoCard function called");
     const card = document.createElement("div");
     card.classList.add("photo-card");
-    const link = document.createElement("a");
-    link.href = image.largeImageURL;
-    link.setAttribute("data-lightbox", "gallery");
-    link.setAttribute("data-title", image.tags);
+    
+    const link = `
+        <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+    `;
 
-    const img = document.createElement("img");
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-    img.loading = "lazy";
+    const info = `
+        <div class="info">
+            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+            <p class="info-item"><b>Views:</b> ${image.views}</p>
+            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+    `;
 
-    const info = document.createElement("div");
-    info.classList.add("info");
-
-    const likes = document.createElement("p");
-    likes.classList.add("info-item");
-    likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
-
-    const views = document.createElement("p");
-    views.classList.add("info-item");
-    views.innerHTML = `<b>Views:</b> ${image.views}`;
-
-    const comments = document.createElement("p");
-    comments.classList.add("info-item");
-    comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
-
-    const downloads = document.createElement("p");
-    downloads.classList.add("info-item");
-    downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
-
-    info.appendChild(likes);
-    info.appendChild(views);
-    info.appendChild(comments);
-    info.appendChild(downloads);
-
-    link.appendChild(img); 
-    card.appendChild(link); 
-    card.appendChild(info);
+    card.innerHTML = link + info;
 
     return card;
-  }
+}
+
 
   function scrollToGallery() {
     const gallery = document.querySelector(".gallery");
